c547c86300a0399234427f668b2f9c87bcdd56f6
Polina <polinaantonovna@yandex.com>
2024-04-11 23:55:44 +0700

    changed locators of main and orders pages

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -15,6 +15,8 @@ class MainPageLocators:
     burger_basket = By.XPATH, './/ul[contains(@class,"BurgerConstructor_basket")]'
     default_order_number = By.XPATH, './/h2[text()="9999"]'
     order_number = By.XPATH, './/h2[contains(@class,"Modal_modal__title_shadow")]'
-    ingredints_count = By.XPATH, './/ul[2]/a[1]/div[1]/p[contains(@class,"counter_counter")]'
+    ingredints_count = By.XPATH, ('.//a[@href="/ingredient/61c0c5a71d1f82001bdaaa72"]'
+                                  '/div[contains(@class,"counter_counter__ZNLkj")]'
+                                  '/p[contains(@class,"counter_counter")]')
     order_preparation_is_started = By.XPATH, './/p[text()="Ваш заказ начали готовить"]'
     order_popup = By.XPATH, './/div[contains(@class,"Modal_orderBox__1xWdi")]'
--- locators/orders_page_locators.py
+++ locators/orders_page_locators.py
@@ -4,8 +4,12 @@ from selenium.webdriver.common.by import By
 class OrdersPageLocators:
     orders_header = By.XPATH, ".//h1[text()='Лента заказов']"
     order_popup = By.XPATH, './/div[contains(@class,"Modal_orderBox__1xWdi")]'
-    last_order_card = By.XPATH, './/ul[1]/li[1][contains(@class,"OrderHistory_listItem__2x95r mb-6")]'
-    orders_done = By.XPATH, './/ul[1][contains(@class,"OrderFeed_orderList")]'
-    orders_all = By.XPATH, './/div[2]/p[contains(@class,"OrderFeed_number__2MbrQ")]'
-    orders_today = By.XPATH, './/div[3]/p[contains(@class,"OrderFeed_number__2MbrQ")]'
-    orders_in_work = By.XPATH, './/ul[2]/li[contains(@class,"text text_type_digits-default")]'
+    last_order_card = By.XPATH, ('.//li[contains(@class,"OrderHistory_listItem__2x95r mb-6")]/'
+                                 'a[@href="/feed/661803989ed280001b3e51ff"]')
+    orders_done = By.XPATH, './/ul[@class="OrderFeed_orderList__cBvyi"]/li[@class="text text_type_digits-default mb-2"]'
+    orders_all = By.XPATH, ('.//div[@class="undefined mb-15"]'
+                            '/p[@class="OrderFeed_number__2MbrQ text text_type_digits-large"]')
+    orders_today = By.XPATH, ('.//div[not(contains(@class, "undefined mb-15"))]/'
+                              'p[@class="OrderFeed_number__2MbrQ text text_type_digits-large"]')
+    orders_in_work = By.XPATH, ('.//ul[@class="OrderFeed_orderListReady__1YFem OrderFeed_orderList__cBvyi"]/'
+                                'li[@class="text text_type_digits-default mb-2"]')
